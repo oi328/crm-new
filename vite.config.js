3e534f5272371dd1bec59087af7ccfafa25546af
Besouhoula <admin@example.com>
2025-12-16 17:47:18 +0200

    Deploy: copy dist contents to root

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ const __dirname = path.dirname(__filename)
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/crm-new/',
+  base: './',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['react-icons/si']
